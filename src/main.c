387ba354b85248ad6de6a174ab3e19ed23fd7fbe
ruiFteixeira <90316091+ruiFteixeira@users.noreply.github.com>
2022-01-12 10:58:43 +0000

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,33 +1,34 @@
-// #include "FreeRTOS.h"
-// #include "task.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include <stdio.h>
 
-// void vTaskA(void *pvParameters);
-// void vTaskB(void *pvParameters);
+void vTaskA(void *pvParameters);
+void vTaskB(void *pvParameters);
 
-// int main(void)
-// {
-//     xTaskCreate(&vTaskA, "Task A", 1024, NULL, 1, NULL);
-//     xTaskCreate(&vTaskB, "Task B", 1024, NULL, 1, NULL);
+int main(void)
+{
+    xTaskCreate(&vTaskA, "Task A", 1024, NULL, 1, NULL);
+    xTaskCreate(&vTaskB, "Task B", 1024, NULL, 1, NULL);
 
-//     vTaskStartScheduler();
+    vTaskStartScheduler();
 
-//     return 0;
-// }
+    return 0;
+}
 
-// void vTaskA(void *pvParameters)
-// {
-//     for (;;)
-//     {
-//         printf("Task A - X\r\n");
-//         vTaskDelay(pdMS_TO_TICKS(1000));
-//     }
-// }
+void vTaskA(void *pvParameters)
+{
+    for (;;)
+    {
+        printf("Task A - X \r\n");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
 
-// void vTaskB(void *pvParameters)
-// {
-//     for (;;)
-//     {
-//         printf("Task B - X\r\n");
-//         vTaskDelay(pdMS_TO_TICKS(1000));
-//     }
-// }
+void vTaskB(void *pvParameters)
+{
+    for (;;)
+    {
+        printf("Task B - X \r\n");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
