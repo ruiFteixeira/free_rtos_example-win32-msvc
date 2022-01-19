f2024801ca48d772391c378ee675d22cdb0acd54
ruiFteixeira <90316091+ruiFteixeira@users.noreply.github.com>
2022-01-19 13:48:54 +0000

    Update main2.c

--- src/main2.c
+++ src/main2.c
@@ -4,38 +4,38 @@
 #include "queue.h"      // inclusão da biblioteca do módulo queue
 #include <string.h>
 
-QueueHandle_t canBufferSPEED;    // declaração da variável de handler da queue
-QueueHandle_t canBufferTEMP;    // declaração da variável de handler da queue
+QueueHandle_t canBufferSPEED;       // declaração da variável de handler da queue, escopo global
+QueueHandle_t canBufferTEMP;        // declaração da variável de handler da queue, escopo global
 
-void vTaskECU(void *pvParameters);    // declaração do prototipo da função das task A
-void vTaskAIRBAG(void *pvParameters);    // declaração do prototipo da função das task B
-void vTaskFAN(void *pvParameters);    // declaração do prototipo da função das task B
+void vTaskECU(void *pvParameters);      // declaração do prototipo da função da task da ECU
+void vTaskAIRBAG(void *pvParameters);   // declaração do prototipo da função da task do Airbag
+void vTaskFAN(void *pvParameters);      // declaração do prototipo da função da task da ventoinha do radiador
 
 int main(void)  // ponto de entrada do programa
 {
-    char buffer[25];
-    canBufferSPEED = xQueueCreate(5,sizeof(buffer));
-    canBufferTEMP = xQueueCreate(5,sizeof(buffer));
+    char buffer[25];    // array de caracteres com 25 posições
+    canBufferSPEED = xQueueCreate(6,sizeof(buffer));    // alocação de memória para a queue, com 6 posições, e cada posição com a dimensão de buffer
+    canBufferTEMP = xQueueCreate(6,sizeof(buffer));     // alocação de memória para a queue, com 6 posições, e cada posição com a dimensão de buffer
 
-    xTaskCreate(&vTaskECU, "ECU Task", 1024, NULL, 1, NULL);    // criar a task
-    xTaskCreate(&vTaskFAN, "FAN Task", 1024, NULL, 1, NULL);    // criar a task
-    xTaskCreate(&vTaskAIRBAG, "Airbag Task", 1024, NULL, 1, NULL);    // criar a task
+    xTaskCreate(&vTaskECU, "ECU Task", 1024, NULL, 1, NULL);        // criar a task
+    xTaskCreate(&vTaskFAN, "FAN Task", 1024, NULL, 1, NULL);        // criar a task
+    xTaskCreate(&vTaskAIRBAG, "Airbag Task", 1024, NULL, 1, NULL);  // criar a task
 
     vTaskStartScheduler();  // escalonador, inicializa o kernel FreeRTOS
 
-    return 0;   // retorna 0 para uma variavel de ambiente do sistema operativo quando termina com sucesso
+    return 0;   // retorna 0 para uma variavel de ambiente do sistema operativo quando termina
 }
 
-void vTaskECU(void *pvParameters)
+void vTaskECU(void *pvParameters) // função da task ECU, para preencher os "buffers de CAN" com os valores de velocidade do carro e temperatura do motor, respetivamente
 {
-    char txBuffer[25];
-    for (int i=0;i<1;i++)
+    char txBuffer[25];      // array de caracteres com 25 posições
+    for (int i=0;i<1;i++)   // (i<1) -> será executada apenas umas vez
     {
-        // sprintf(txBuffer, "Speed+100");
-        // xQueueSend(canBufferSPEED, (void*) txBuffer, (TickType_t) 0);
+        // sprintf(txBuffer, "Speed+112");     // escrever para a variavel txBuffer, a velocidade em km/h, com o prefixo Speed para permitir posteriormente fazer o parse 
+        // xQueueSend(canBufferSPEED, (void*) txBuffer, (TickType_t) 0);   // escrever o conteúdo de txBuffer para a queue
         // sprintf(txBuffer, "Speed+120");
         // xQueueSend(canBufferSPEED, (void*) txBuffer, (TickType_t) 0);
-        // sprintf(txBuffer, "Speed+110");
+        // sprintf(txBuffer, "Speed+113");
         // xQueueSend(canBufferSPEED, (void*) txBuffer, (TickType_t) 0);
         // sprintf(txBuffer, "Speed+110");
         // xQueueSend(canBufferSPEED, (void*) txBuffer, (TickType_t) 0);
@@ -44,11 +44,11 @@ void vTaskECU(void *pvParameters)
         // sprintf(txBuffer, "Speed+0");
         // xQueueSend(canBufferSPEED, (void*) txBuffer, (TickType_t) 0);
 
-        // sprintf(txBuffer, "Temp+100");
+        // sprintf(txBuffer, "Temp+110");  // escrever para a variavel txBuffer, a temperatura do motor em graus celsius, com o prefixo Temp (temperatura) para permitir posteriormente fazer o parse
+        // xQueueSend(canBufferTEMP, (void*) txBuffer, (TickType_t) 0);    // escrever o conteúdo de txBuffer para a queue
+        // sprintf(txBuffer, "Temp+113");
         // xQueueSend(canBufferTEMP, (void*) txBuffer, (TickType_t) 0);
-        // sprintf(txBuffer, "Temp+105");
-        // xQueueSend(canBufferTEMP, (void*) txBuffer, (TickType_t) 0);
-        // sprintf(txBuffer, "Temp+120");
+        // sprintf(txBuffer, "Temp+118");
         // xQueueSend(canBufferTEMP, (void*) txBuffer, (TickType_t) 0);
         // sprintf(txBuffer, "Temp+105");
         // xQueueSend(canBufferTEMP, (void*) txBuffer, (TickType_t) 0);
@@ -58,56 +58,56 @@ void vTaskECU(void *pvParameters)
         // xQueueSend(canBufferTEMP, (void*) txBuffer, (TickType_t) 0);
 
         printf(" ECU Task - executing \r\n");
-        vTaskDelay(pdMS_TO_TICKS(2000));
+        vTaskDelay(pdMS_TO_TICKS(2000));    // delay de 2 segundos
     }
 }
 
-void vTaskAIRBAG(void *pvParameters)
+void vTaskAIRBAG(void *pvParameters)    // função da task do Airbag, para ler os "buffers de CAN" com os valores de velocidade do carro, e fazer atuar o airbag, caso se verifique uma desaceleração superior a 50 km/h entre dois valores consecutivos
 {
-    char rxBuffer[25];
-    // char *token1 = NULL;
-    // int previousSpeed = 0;
-    // int actualSpeed = 0;
-    while(1){
-        if(0!=canBufferSPEED){
-            xQueueReceive(canBufferSPEED, (void*) rxBuffer, (TickType_t) 4);
-            printf("Speed CAN readings: %s\r\n",rxBuffer);
-            // if(NULL != strstr(rxBuffer, "Speed")){
-            //     token1 = strtok(rxBuffer,"+");
+    char rxBuffer[25];      // array de caracteres que será utilizado para armazenar a string lida da queue
+    char *token1 = NULL;    // apontador do tipo char que será utilizado no processo de parsing da strind lida da queue
+    int previousSpeed = 0;  // variavel para guardar o valor de velocidade anteriormente lido
+    int actualSpeed = 0;    // variavel para guardar o valor de velocidade atualmente lido
+    while(1){   // ciclo infinito
+        if(0!=canBufferSPEED){  // caso a queue não esteja vazia
+            xQueueReceive(canBufferSPEED, (void*) rxBuffer, (TickType_t) 4);    // ler a queue e armazenar o valor lido na variável rxBuffer
+            printf("Speed CAN readings: %s\r\n",rxBuffer);  // imprimir para o ecrã o conteúdo de rxBuffer
+            // if(NULL != strstr(rxBuffer, "Speed")){          // se for encontrado em rxBuffer a string "Speed"
+            //     token1 = strtok(rxBuffer,"+");              // fazer o parsing de "Speed+valor" para extrair apenas o valor, e deixar cair o prefixo "Speed"
             //     token1 = strtok(NULL,"+");
-            //     strcpy(rxBuffer,token1);
-            //     actualSpeed = atoi(rxBuffer);
-            //     if(50<(previousSpeed - actualSpeed)){
-            //         printf("!!! activate aribag !!!: %s\r\n",rxBuffer);
-            //         vTaskDelay(pdMS_TO_TICKS(1000));
+            //     strcpy(rxBuffer,token1);                    // copiar o valor extraido em token1, para substituir no rxBuffer e ficar apenas com o valor
+            //     actualSpeed = atoi(rxBuffer);               // converter o valor de char para int, e atribuir à variavel actualSpeed
+            //     if(50<(previousSpeed - actualSpeed)){       // caso se verifique uma "desaceleração" superior a 50 km/h
+            //         printf("!!! activate airbag !!!: %s\r\n",rxBuffer); // printf de "ativar o airbag" para simular o que seria um comando de atuação do airbag
+            //         vTaskDelay(pdMS_TO_TICKS(1000));        // delay de 1 segundo
             //     }
-            //     previousSpeed = actualSpeed;
+            //     previousSpeed = actualSpeed;    // passar o valor de velocidade lido nesta iteração, para ser utilizado na próxima iteração, na qual será o "valor anterior" para comparação
             // }
-            vTaskDelay(pdMS_TO_TICKS(1000));
+            vTaskDelay(pdMS_TO_TICKS(1000));    // delay de 1 segundo
         }
     }
 }
 
-void vTaskFAN(void *pvParameters)
+void vTaskFAN(void *pvParameters)   // função da task da ventoinha do cooler, para ler o "buffer de CAN" com os valores de temperatura do motor do carro, e ligar a ventoinha do radiador, caso a temperatura exceda os 115 graus celsius
 {
-    char rxBuffer[25];
-    // char *token1 = NULL;
-    // int temperature = 0;
-    while(1){
-        if(0!=canBufferTEMP){
-            xQueueReceive(canBufferTEMP, (void*) rxBuffer, (TickType_t) 4);
-            printf("Temperature CAN readings: %s\r\n",rxBuffer);
-            // if(NULL != strstr(rxBuffer, "Temp")){
-            //     token1 = strtok(rxBuffer,"+");
+    char rxBuffer[25];      // array de caracteres que será utilizado para armazenar a string lida da queue
+    char *token1 = NULL;    // apontador do tipo char que será utilizado no processo de parsing da strind lida da queue
+    int temperature = 0;    // variavel para guardar o valor de temperatura do motor
+    while(1){   // ciclo infinito
+        if(0!=canBufferTEMP){   // caso a queue não esteja vazia
+            xQueueReceive(canBufferTEMP, (void*) rxBuffer, (TickType_t) 4); // ler a queue e armazenar o valor lido na variável rxBuffer
+            printf("Temperature CAN readings: %s\r\n",rxBuffer);    // imprimir para o ecrã o conteúdo de rxBuffer
+            // if(NULL != strstr(rxBuffer, "Temp")){       // se for encontrado em rxBuffer a string "Temp" (de temperatura)
+            //     token1 = strtok(rxBuffer,"+");          // fazer o parsing de "Temp+valor" para extrair apenas o valor, e deixar cair o prefixo "Temp"
             //     token1 = strtok(NULL,"+");
-            //     strcpy(rxBuffer,token1);
-            //     temperature = atoi(rxBuffer);
-            //     if(115<temperature){
-            //         printf("!!! turn on the cooler FAN !!!: %s\r\n",rxBuffer);
-            //         vTaskDelay(pdMS_TO_TICKS(1000));
+            //     strcpy(rxBuffer,token1);                // copiar o valor extraido em token1, para substituir no rxBuffer e ficar apenas com o valor
+            //     temperature = atoi(rxBuffer);           // converter o valor de char para int, e atribuir à variavel temperature
+            //     if(115<temperature){                    // caso a temperatura do motor exceda os 115 graus celsius
+            //         printf("!!! turn on the cooler FAN !!!: %s\r\n",rxBuffer);  // printf de "ligar a ventoinha do radiador" para simular o que seria um comando de atuação da ventoinha
+            //         vTaskDelay(pdMS_TO_TICKS(1000));    // delay de 1 segundo
             //     }
             // }
-            vTaskDelay(pdMS_TO_TICKS(1000));
+            vTaskDelay(pdMS_TO_TICKS(1000));    // delay de 1 segundo
         }
     }
 }
